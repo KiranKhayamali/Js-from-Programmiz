8f7f57c3a606048c4882af29ac904bac21186833
Kiran Khayamali <kirankhayamali@gmail.com>
2024-09-30 16:40:45 +0545

    Learning tenary operations

---
+++ Intermediate level/TenaryOperators.js
@@ -0,0 +1,9 @@
+// get the two numbers from the user
+let number1 = prompt("Enter the first number:");
+let number2 = prompt("Enter the second number:");
+
+// determine the larger number using a ternary operator
+let larger =(number1 > number2) ? number1 : number2;
+
+// output the result
+console.log(larger);
