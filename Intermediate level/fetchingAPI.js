59bc6d4bf817d8c61c374b43b8caef6dae177fcd
Kiran Khayamali <kirankhayamali@gmail.com>
2024-10-04 17:43:13 +0545

    Fetching only the movies names from the api

--- Intermediate level/fetchingAPI.js
+++ Intermediate level/fetchingAPI.js
@@ -5,3 +5,13 @@ let api = fetch("https://api.programiz.pro/api/Demo/javascript/movies");
 api.then(response => {console.log(response);});
 
 
+//only displaying names of movies from the given api
+const endpointURL = 'https://api.programiz.pro/api/Demo/javascript/movies';
+let fetchData = fetch(endpointURL);
+fetchData
+    .then(response => response.json()) //the response from the api is converted into json format first 
+    .then(data =>
+        {
+            data.movies.forEach(movie => console.log(movie.name)); //extracting each movie name seperately
+        }
+    );
