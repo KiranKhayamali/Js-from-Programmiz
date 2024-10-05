86b8506e9bfe061eaaceedeb32062134c234b018
Kiran Khayamali <kirankhayamali@gmail.com>
2024-10-05 16:41:52 +0545

    using async function to fetch data and print it

--- Intermediate level/async.js
+++ Intermediate level/async.js
@@ -6,3 +6,11 @@ async function myAsync() {
 }
 console.log(myAsync()) // Output: Promise { <pending> }
 myAsync().then((data) => console.log(data))
+
+//Task 1:Create an async function named getData that fetches data from a URL and prints the data.
+async function getData(){
+    let data = await fetch("https://api.programiz.pro/api/Demo/javascript/valid");
+    let text = await data.text();
+    console.log(text);
+}
+getData(); 
