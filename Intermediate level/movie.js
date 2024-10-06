3151a6f92f8576c93b1fb8bfe84cb1e81ab5306a
Kiran Khayamali <kirankhayamali@gmail.com>
2024-10-06 16:04:49 +0545

    Displaying the movies name that have 9 or higher rating

--- Intermediate level/movie.js
+++ Intermediate level/movie.js
@@ -7,11 +7,20 @@ function fetchMovieData(url) {
         });
 }
 
+function getHighRatedMovies(movies){
+    return movies
+        .filter(movie => movie.rating >= 9)
+        .map(movie => movie.name);
+}
+
 async function main() {
     const URL = "https://api.programiz.pro/api/Demo/javascript/movies";
     const movieData = await fetchMovieData(URL);
 
     console.log(movieData);
+
+    console.log("\nMovies that have 9 or higher rating:")    
+    console.log(getHighRatedMovies(movieData["movies"]));
 }
 
 main();
