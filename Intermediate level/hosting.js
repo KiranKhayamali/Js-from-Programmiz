e38e9cc8648b697af70da4c0dd02b93d2cccb47e
Kiran Khayamali <kirankhayamali@gmail.com>
2024-10-06 16:01:16 +0545

    learning about variable hosting

---
+++ Intermediate level/hosting.js
@@ -0,0 +1,13 @@
+//variable hosting
+function printMessage(){
+    message = "Hello";
+    
+    // use the message variable before declaration
+    console.log(message); // Output: Hello
+    
+    // variable declaration using var keyword
+    var message;
+ 
+}
+
+printMessage();
