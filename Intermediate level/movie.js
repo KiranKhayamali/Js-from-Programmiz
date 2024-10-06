978360948f2411288c2fc199b032fe3ce7b498e5
Kiran Khayamali <kirankhayamali@gmail.com>
2024-10-06 16:07:55 +0545

    Error handling

--- Intermediate level/movie.js
+++ Intermediate level/movie.js
@@ -2,12 +2,14 @@ function fetchMovieData(url) {
     return fetch(url)
         .then(response =>  response.json())
         .catch(error => {
-            console.error("There was a problem with the fetch operation:");
-            return null;
+            throw new Error("there was a problem with the fetch operation: " );
         });
 }
 
 function getHighRatedMovies(movies){
+    if (!Array.isArray(movies)) {
+        throw new Error("invalid data format: movies should be an array.");
+    }
     return movies
         .filter(movie => movie.rating >= 9)
         .map(movie => movie.name);
@@ -15,12 +17,22 @@ function getHighRatedMovies(movies){
 
 async function main() {
     const URL = "https://api.programiz.pro/api/Demo/javascript/movies";
-    const movieData = await fetchMovieData(URL);
+    try{
+        const movieData = await fetchMovieData(URL);
+        if(!movieData){
+            throw new Error("Failed to fetch movie data.")
+        }
 
-    console.log(movieData);
+        if(!Array.isArray(movieData.movies)){
+            throw new Error("Invalid movie data format.")
+        }
+        console.log(movieData);
 
-    console.log("\nMovies that have 9 or higher rating:")    
-    console.log(getHighRatedMovies(movieData["movies"]));
+        console.log("\nMovies that have 9 or higher rating:")    
+        console.log(getHighRatedMovies(movieData["movies"]));
+    }catch(error){
+        console.error("An error occured!!")
+    }   
 }
 
 main();
